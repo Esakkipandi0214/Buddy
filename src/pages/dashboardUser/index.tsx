37e11be0d67841a0abf1e49bbb35544ab05cf2e8
Esakkipandi0214 <praveenpandian01@gmail.com>
2024-09-19 12:34:52 +0530

    update

--- src/pages/dashboardUser/index.tsx
+++ src/pages/dashboardUser/index.tsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import Dashboard from '../../components/Dashboard/dashboard'
-import Layout from '@/components/staticComponents/layout'
+import React, { useEffect, useState } from 'react';
+import Dashboard from '../../components/Dashboard/dashboard';
+import Layout from '@/components/staticComponents/layout';
+import { useRouter } from 'next/router';
 
-const index = () => {
+const Index = () => {
+  const router = useRouter();
+  const [access, setAccess] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    redirect();
+  }, [router]);
+  
+  const redirect = () => {
+    const userUid = localStorage.getItem('userUid');
+    if (!userUid) {
+      setAccess(false);
+      router.push('/');
+    } else {
+      setAccess(true);
+    }
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-lg font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-green-400 to-purple-500 animate-pulse">
+          Loading, Buddy...
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
-    <Layout>
-    <Dashboard/>
-    </Layout>
+      {access ? (
+        <Layout>
+          <Dashboard />
+        </Layout>
+      ) : null}
     </>
-  )
-}
+  );
+};
 
-export default index
+export default Index;
